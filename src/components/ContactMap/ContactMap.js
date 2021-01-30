26a5f3637f151d8e4428e68c9ffc099b60cb3030
zofianatanek <zofia.natanek@gmail.com>
2021-01-30 18:48:07 +0100

    Fix map

--- src/components/ContactMap/ContactMap.js
+++ src/components/ContactMap/ContactMap.js
@@ -13,7 +13,7 @@ export default class ContactMap extends Component {
           style={{ width: "700px", height: "500px" }}
         >
           <TileLayer
-            url={`${process.env.GATSBY_API_URL}`}
+            url={`https://api.mapbox.com/styles/v1/sophijaful/ckkjk70lb1wuu17nwji2bwi5o/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.GATSBY_API_URL}`}
             attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
           />
           <Marker position={this.position}>
